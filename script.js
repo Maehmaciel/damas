2b2bfcea19edf8663c71b7d6868ea1e23459f667
Maria Maciel <mariaeugeniamaciel@gmail.com>
2020-07-08 09:16:54 -0300

    refatora funçao calculatePosition

--- script.js
+++ script.js
@@ -10,53 +10,72 @@ function drag(ev) {
 }
 
 function validaPosicao(posicao) {
-  if(document.getElementById(posicao) != null){
-  return true
-  } 
+  if (document.getElementById(posicao) != null) {
+    return true
+  }
   return false
 }
 
 
 function validaPonto(time, posicao, prox) {
-  let time_=""
-  let t=document.getElementById(posicao).childNodes
-  for(let a in t){
-    if(!!t[a].className)
-    time_=t[a].className.split(" ")[1]
+  let time_ = ""
+  let t = document.getElementById(posicao).childNodes
+  for (let a in t) {
+    if (!!t[a].className)
+      time_ = t[a].className.split(" ")[1]
   }
-  if ((document.getElementById(posicao)!=null) && (document.getElementById(prox) !=null)) {
+  if ((document.getElementById(posicao) != null) && (document.getElementById(prox) != null)) {
     if ((document.getElementById(posicao).childNodes.length > 0) && (document.getElementById(prox).childNodes.length == 0)) {
-      if(time_!=time)
+      if (time_ != time)
         return true
-     
+
     }
   }
-    return false
+  return false
 
 
 }
 
-function calculatePositionRed(antigo) {
+function calculatePosition(antigo, time) {
   let letra = antigo.split('')[0]
   let numero = Number(antigo.split('')[1])
   let response = []
-  let proxLetra = posicao.indexOf(letra) + 1
-  let pos1 = posicao[proxLetra] + (numero + 1)
-  let pos2 = posicao[proxLetra] + (numero - 1)
-  let proxPos1 = posicao[proxLetra + 1] + (numero + 2)
-  let proxPos2 = posicao[proxLetra + 1] + (numero - 2)
+  let proxLetra=0
+  let pos1=""
+  let pos2=""
+  let proxPos1=""
+  let proxPos2=""
+
+  switch (time) {
+    case 'red':
+      proxLetra = posicao.indexOf(letra) + 1
+      pos1 = posicao[proxLetra] + (numero + 1)
+      pos2 = posicao[proxLetra] + (numero - 1)
+      proxPos1 = posicao[proxLetra + 1] + (numero + 2)
+      proxPos2 = posicao[proxLetra + 1] + (numero - 2)
+      break;
+    case 'blue':
+      proxLetra = posicao.indexOf(letra) - 1
+      pos1 = posicao[proxLetra] + (numero - 1)
+      pos2 = posicao[proxLetra] + (numero + 1)
+      proxPos1 = posicao[proxLetra - 1] + (numero - 2)
+      proxPos2 = posicao[proxLetra - 1] + (numero + 2)
+      break;
+    default:
+      break;
+  }
   if (validaPosicao(pos1) && validaPosicao(pos2)) {
-    if (validaPonto('red', pos1, proxPos1)) {
+    if (validaPonto(time, pos1, proxPos1)) {
       response.push(proxPos1)
-      response.push({pos:proxPos1,remove:pos1})
+      response.push({ pos: proxPos1, remove: pos1 })
     }
     else {
       response.push(pos1)
     }
 
-    if (validaPonto('red', pos2, proxPos2)) {
+    if (validaPonto(time, pos2, proxPos2)) {
       response.push(proxPos2)
-      response.push({pos:proxPos2,remove:pos2})
+      response.push({ pos: proxPos2, remove: pos2 })
     }
     else {
       response.push(pos2)
@@ -64,18 +83,18 @@ function calculatePositionRed(antigo) {
 
   }
   else if (validaPosicao(pos1)) {
-    if (validaPonto('red', pos1, proxPos1)) {
+    if (validaPonto(time, pos1, proxPos1)) {
       response.push(proxPos1)
-      response.push({pos:proxPos1,remove:pos1})
+      response.push({ pos: proxPos1, remove: pos1 })
     }
     else {
       response.push(pos1)
     }
   }
   else if (validaPosicao(pos2)) {
-    if (validaPonto('red', pos2, proxPos2)) {
+    if (validaPonto(time, pos2, proxPos2)) {
       response.push(proxPos2)
-      response.push({pos:proxPos2,remove:pos2})
+      response.push({ pos: proxPos2, remove: pos2 })
     }
     else {
       response.push(pos2)
@@ -85,54 +104,7 @@ function calculatePositionRed(antigo) {
   return response
 }
 
-function calculatePositionBlue(antigo) {
-  let letra = antigo.split('')[0]
-  let numero = Number(antigo.split('')[1])
-  let response = []
-  let proxLetra = posicao.indexOf(letra) - 1
-  let pos1 = posicao[proxLetra] + (numero - 1)
-  let pos2 = posicao[proxLetra] + (numero + 1)
-  let proxPos1 = posicao[proxLetra - 1] + (numero - 2)
-  let proxPos2 = posicao[proxLetra - 1] + (numero + 2)
-  if (validaPosicao(pos1) && validaPosicao(pos2)) {
-    if (validaPonto('blue', pos1, proxPos1)) {
-      response.push(proxPos1)
-      response.push({pos:proxPos1,remove:pos1})
-    }
-    else {
-      response.push(pos1)
-    }
 
-    if (validaPonto('blue', pos2, proxPos2)) {
-      response.push(proxPos2)
-      response.push({pos:proxPos2,remove:pos2})
-    }
-    else {
-      response.push(pos2)
-    }
-
-  }
-  else if (validaPosicao(pos1)) {
-    if (validaPonto('blue', pos1, proxPos1)) {
-      response.push(proxPos1)
-      response.push({pos:proxPos1,remove:pos1})
-    }
-    else {
-      response.push(pos1)
-    }
-  }
-  else if (validaPosicao(pos2)) {
-    if (validaPonto('blue', pos2, proxPos2)) {
-      response.push(proxPos2)
-      response.push({pos:proxPos2,remove:pos2})
-    }
-    else {
-      response.push(pos2)
-    }
-  }
-
-  return response
-}
 
 
 function drop(ev) {
@@ -155,12 +127,8 @@ function drop(ev) {
     return
   }
 
-  if (time === "red") {
-    proximas = calculatePositionRed(antigo.id)
-  }
-  else {
-    proximas = calculatePositionBlue(antigo.id)
-  }
+    proximas = calculatePosition(antigo.id,time)
+
 
 
   for (let p in proximas) {
@@ -171,8 +139,8 @@ function drop(ev) {
         return
       }
     } else {
-      if(proximas[p].pos===novo.id)
-      remove=proximas[p].remove
+      if (proximas[p].pos === novo.id)
+        remove = proximas[p].remove
     }
 
   }
@@ -189,7 +157,7 @@ function drop(ev) {
       divRemove.removeChild(divRemove.lastChild);
     }
   }
-  socket.emit('chat msg',{bolinha:bolinha.id,lugar:novo.id, remove})
+  socket.emit('chat msg', { bolinha: bolinha.id, lugar: novo.id, remove })
 
 
 }
